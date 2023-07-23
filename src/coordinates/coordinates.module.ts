b853f917b448bba6b0af655d74f7f8289544d8be
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-23 09:01:57 -0400

    Add 'Coordinates' Controller & Config

--- src/coordinates/coordinates.module.ts
+++ src/coordinates/coordinates.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CoordinatesController } from './coordinates.controller';
 
-@Module({})
+@Module({
+  controllers: [CoordinatesController]
+})
 export class CoordinatesModule {}
