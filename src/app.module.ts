fbb263aec60df1a7ddffa6332227ee175b6fe0a1
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-24 07:19:11 -0400

    Feat: Add `TypeOrmModule`, Integrating SQLite3 DB
    - Import `TypeOrmModule` from NestJS 'typeorm' package
    - Add `TypeOrmModule` to Module 'imports'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,7 +7,17 @@ import { CoordinatesModule } from './coordinates/coordinates.module';
 import { MaterialsModule } from './materials/materials.module';
 
 @Module({
-  imports: [UsersModule, CoordinatesModule, MaterialsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    CoordinatesModule,
+    MaterialsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
