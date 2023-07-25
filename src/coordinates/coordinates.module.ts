199346753d53c77cb6decfb705050299944e733d
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-25 12:00:43 -0400

    Feat: Add `coordinate.entity` To `module`
    - Import `TypeOrmModule` from NestJS 'typeorm' package
    - Import 'Coordinate' class
    - Add 'Coordinate' as a feature to the Module imports
    
    Closes #18

--- src/coordinates/coordinates.module.ts
+++ src/coordinates/coordinates.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoordinatesController } from './coordinates.controller';
 import { CoordinatesService } from './coordinates.service';
+import { Coordinate } from './entities/coordinate.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coordinate])],
   controllers: [CoordinatesController],
-  providers: [CoordinatesService]
+  providers: [CoordinatesService],
 })
 export class CoordinatesModule {}
