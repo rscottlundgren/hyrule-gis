68cd104b835157886e459193b097569c317027e5
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-25 12:07:19 -0400

    Feat: Add 'Coordinate' Entity to `app.module.ts`
    - Import 'Coordinate' entity into `app.module.ts`
    - Add 'Coordinate' to Module's imported 'entities'
    
    Closes #19

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CoordinatesModule } from './coordinates/coordinates.module';
 import { MaterialsModule } from './materials/materials.module';
+import { Coordinate } from './coordinates/entities/coordinate.entity';
 import { User } from './users/entities/user.entity';
 
 @Module({
@@ -12,7 +13,7 @@ import { User } from './users/entities/user.entity';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [Coordinate, User],
       synchronize: true,
     }),
     UsersModule,
