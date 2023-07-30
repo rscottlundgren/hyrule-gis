11efb39c750a86a6247d1c53a52edfb31d13b450
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-30 10:58:48 -0400

    Add 'common-locations' Service to Module

--- src/common-locations/common-locations.module.ts
+++ src/common-locations/common-locations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommonLocationsController } from './common-locations.controller';
+import { CommonLocationsService } from './common-locations.service';
 
 @Module({
-  controllers: [CommonLocationsController]
+  controllers: [CommonLocationsController],
+  providers: [CommonLocationsService]
 })
 export class CommonLocationsModule {}
