681b187e7bf4fb6c147dd1cd563d25e967702032
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-30 10:57:34 -0400

    Add 'common-locations' Controller to Module

--- src/common-locations/common-locations.module.ts
+++ src/common-locations/common-locations.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CommonLocationsController } from './common-locations.controller';
 
-@Module({})
+@Module({
+  controllers: [CommonLocationsController]
+})
 export class CommonLocationsModule {}
