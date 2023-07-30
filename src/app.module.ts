6a600e5c1f11f03bd0d1633a0d7c40c7487f9c8d
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-07-30 12:35:00 -0400

    Import 'CommonLocation' Entity & Add To Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+
+// Modules
+import { CommonLocationsModule } from './common-locations/common-locations.module';
 import { CoordinatesModule } from './coordinates/coordinates.module';
 import { MaterialsModule } from './materials/materials.module';
+import { UsersModule } from './users/users.module';
+
+// Entities
+import { CommonLocation } from './common-locations/entities/common-location.entity';
 import { Coordinate } from './coordinates/entities/coordinate.entity';
 import { Material } from './materials/entities/material.entity';
 import { User } from './users/entities/user.entity';
-import { CommonLocationsModule } from './common-locations/common-locations.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Coordinate, Material, User],
+      entities: [CommonLocation, Coordinate, Material, User],
       synchronize: true,
     }),
     UsersModule,
