9e53847297baa5dbd4bb3ac0ba37548b4008d30f
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-03 11:34:35 -0400

    Add 'constructor', 'Post' Method To 'controller'
    - Import 'Body' & 'Post' decorators
    - Import 'CreateCommonLocationDto'
    - Import 'CommonLocationsService'
    - Add 'constructor', dependency inject service
    - Add 'Post' decorator, instantiating 'createCommonLocation' method

--- src/common-locations/common-locations.controller.ts
+++ src/common-locations/common-locations.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateCommonLocationDto } from './dtos/create-common-location.dto';
+import { CommonLocationsService } from './common-locations.service';
 
 @Controller('common-locations')
-export class CommonLocationsController {}
+export class CommonLocationsController {
+  constructor(
+    private readonly commonLocationsService: CommonLocationsService,
+  ) {}
+
+  @Post()
+  createCommonLocation(@Body() body: CreateCommonLocationDto) {
+    this.commonLocationsService.create(body.name);
+  }
+}
