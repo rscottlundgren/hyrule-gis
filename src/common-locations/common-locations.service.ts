5aa82affa0e038436fe1e8d11e64ebedbf6950c1
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-03 09:18:58 -0400

    Connect 'service' To 'repo'; Configure 'create'
    - Import `InjectRepository` decorator
    - Import `Repository` class
    - Import `CommonLocation` entity
    - Add a `constructor` to service class, establishing repo connection
    - Add `create()` method to 'upsert' a record into the repo

--- src/common-locations/common-locations.service.ts
+++ src/common-locations/common-locations.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CommonLocation } from './entities/common-location.entity';
 
 @Injectable()
-export class CommonLocationsService {}
+export class CommonLocationsService {
+  constructor(
+    @InjectRepository(CommonLocation)
+    private readonly repo: Repository<CommonLocation>,
+  ) {}
+
+  create(name: string) {
+    const commonLocation = this.repo.create({ name });
+
+    return this.repo.save(commonLocation);
+  }
+}
