86e50ea518cef7595bca914c57847d195edb6646
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-09 14:16:07 -0400

    Add Imports, Create 'createUniqueCookingEffect()'

--- src/unique-cooking-effects/unique-cooking-effects.controller.ts
+++ src/unique-cooking-effects/unique-cooking-effects.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UniqueCookingEffectsService } from './unique-cooking-effects.service';
+import { CreateUniqueCookingEffectDto } from './dtos/create-unique-cooking-effect.dto';
 
 @Controller('unique-cooking-effects')
-export class UniqueCookingEffectsController {}
+export class UniqueCookingEffectsController {
+  constructor(
+    private readonly uniqueCookingEffectsService: UniqueCookingEffectsService,
+  ) {}
+
+  @Post()
+  createUniqueCookingEffect(@Body() body: CreateUniqueCookingEffectDto) {
+    return this.uniqueCookingEffectsService.create(body.name, body.description);
+  }
+}
