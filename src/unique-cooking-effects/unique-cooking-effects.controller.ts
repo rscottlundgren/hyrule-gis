5ae4a9d2dffcab98f77425b1d9b96e7e0776d543
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-10 14:15:29 -0400

    Add 'find...()', 'findAll...()' Methods To 'controller'

--- src/unique-cooking-effects/unique-cooking-effects.controller.ts
+++ src/unique-cooking-effects/unique-cooking-effects.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UniqueCookingEffectsService } from './unique-cooking-effects.service';
 import { CreateUniqueCookingEffectDto } from './dtos/create-unique-cooking-effect.dto';
 
@@ -11,5 +11,15 @@ export class UniqueCookingEffectsController {
   @Post()
   createUniqueCookingEffect(@Body() body: CreateUniqueCookingEffectDto) {
     return this.uniqueCookingEffectsService.create(body.name, body.description);
+  }
+
+  @Get()
+  findAllUniqueCookingEffects() {
+    return this.uniqueCookingEffectsService.find();
+  }
+
+  @Get('/:id')
+  findUniqueCookingEffect(@Param('id') id: string) {
+    return this.uniqueCookingEffectsService.findOne(parseInt(id));
   }
 }
