dfb53f9a1d2ecf96d83600391634cd22b677aba3
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-13 14:10:52 -0400

    Add 'create...()' Method To 'controller'

--- src/materials/materials.controller.ts
+++ src/materials/materials.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { MaterialsService } from './materials.service';
+import { CreateMaterialDto } from './dtos/create-material.dto';
 
 @Controller('materials')
-export class MaterialsController {}
+export class MaterialsController {
+  constructor(private readonly materialsService: MaterialsService) {}
+
+  @Post()
+  createMaterial(@Body() body: CreateMaterialDto) {
+    return this.materialsService.create(
+      body.name,
+      body.description,
+      body.fuse_attack_power,
+      body.hearts_recovered,
+      body.unique_cooking_effect,
+      body.common_locations,
+      body.tradeable,
+    );
+  }
+}
