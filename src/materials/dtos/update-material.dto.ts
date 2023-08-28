bcb1d38b219b2cf829323bdbd01d0f3c2260f15c
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-28 13:05:46 -0400

    Remove Entity Import, Update Field Type
    - Remove 'UniqueCookingEffect' entity import
    - Change 'unique_cooking_effect' type from entity to string

--- src/materials/dtos/update-material.dto.ts
+++ src/materials/dtos/update-material.dto.ts
@@ -1,5 +1,4 @@
 import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
-import { UniqueCookingEffect } from '../../unique-cooking-effects/entities/unique-cooking-effect.entity';
 
 export class UpdateMaterialDto {
   @IsNumber()
@@ -24,7 +23,7 @@ export class UpdateMaterialDto {
 
   @IsString()
   @IsOptional()
-  readonly unique_cooking_effect: UniqueCookingEffect;
+  readonly unique_cooking_effect: string;
 
   @IsString({ each: true })
   @IsOptional()
