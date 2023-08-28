4932b47cb48a7372fb230bb0c0d41f83169fc658
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-28 13:04:30 -0400

    Update Imports & Two Fields + Decorators
    - Import 'ManyToMany' decorator
    - Import 'CommonLocation' entity
    - Update 'unique_cooking_effect', adding 'cascade' field functionality
    - Replace '@Column()' on 'common_locations' field with '@ManyToMany()`
    - Add parameters to '@ManyToMany()'
    - Change 'common_locations' type to array of 'CommonLocation' entities

--- src/materials/entities/material.entity.ts
+++ src/materials/entities/material.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, Column, PrimaryColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToMany, ManyToOne, PrimaryColumn } from 'typeorm';
 import { UniqueCookingEffect } from '../../unique-cooking-effects/entities/unique-cooking-effect.entity';
+import { CommonLocation } from '../../common-locations/entities/common-location.entity';
 
 @Entity()
 export class Material {
@@ -21,12 +22,16 @@ export class Material {
   @ManyToOne(
     () => UniqueCookingEffect,
     (uniqueCookingEffect) => uniqueCookingEffect.materials,
-    { nullable: true },
+    { nullable: true, cascade: ['update', 'remove', 'soft-remove'] },
   )
   unique_cooking_effect: UniqueCookingEffect;
 
-  @Column('json')
-  common_locations: string[];
+  @ManyToMany(
+    () => CommonLocation,
+    (commonLocation) => commonLocation.materials,
+    { cascade: ['update', 'remove', 'soft-remove'] },
+  )
+  common_locations: CommonLocation[];
 
   @Column({ nullable: true })
   tradeable: boolean;
