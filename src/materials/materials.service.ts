ba96ca6932bf74bb011cd41a256855ef34a63e22
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-08-28 13:12:24 -0400

    Update Imports, Add Business Logic
    - Import 'CommonLocationsService'
    - Import 'UpdateMaterialDto'
    - Update constructor parameters with 'CommonLocationsService'
    - Remove comments from 'create()' method
    - Update individual service calls in 'create()' with short circuit
    - Add 'common_locations' field to return in 'find()' method
    - Amend 'update()' method parameters
    - Amend 'update()' method with short circuits to business logic

--- src/materials/materials.service.ts
+++ src/materials/materials.service.ts
@@ -7,12 +7,16 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Material } from './entities/material.entity';
 import { UniqueCookingEffectsService } from 'src/unique-cooking-effects/unique-cooking-effects.service';
+import { CommonLocationsService } from '../common-locations/common-locations.service';
+import { UpdateMaterialDto } from './dtos/update-material.dto';
 
 @Injectable()
 export class MaterialsService {
   constructor(
-    @InjectRepository(Material) private readonly repo: Repository<Material>,
-    private readonly uceService: UniqueCookingEffectsService,
+    @InjectRepository(Material)
+    private readonly repoMaterial: Repository<Material>,
+    private readonly srvcUniqueCookingEffect: UniqueCookingEffectsService,
+    private readonly srvcCommonLocation: CommonLocationsService,
   ) {}
 
   async create(
@@ -25,23 +29,20 @@ export class MaterialsService {
     tradeable: boolean,
     unique_cooking_effect: string,
   ) {
-    /**
-     * 1. Check to see if we can return a value for the unique_cooking_effect
-     */
-    const potentialUniqueCookingEffect = await this.uceService.findByName(
-      unique_cooking_effect,
-    );
+    const potentialUniqueCookingEffect =
+      unique_cooking_effect &&
+      (await this.srvcUniqueCookingEffect.findByName(unique_cooking_effect));
 
-    /**
-     * 3. Check to see if we can return a value for the material
-     */
-    const potentialMaterial = await this.repo.find({
+    const potentialCommonLocations =
+      common_locations.length !== 0 &&
+      (await Promise.all(
+        await this.srvcCommonLocation.findByName(common_locations),
+      ));
+
+    const potentialMaterial = await this.repoMaterial.find({
       where: { name },
     });
 
-    /**
-     * 4. Validate presence of material value; throw err if present
-     */
     if (potentialMaterial.length !== 0) {
       const { id, name } = potentialMaterial[0];
 
@@ -50,26 +51,24 @@ export class MaterialsService {
       );
     }
 
-    /**
-     * 5. Create the material object
-     */
-    const material = this.repo.create({
+    const material = this.repoMaterial.create({
       id,
       name,
       description,
       fuse_attack_power,
       hearts_recovered,
-      common_locations,
       tradeable,
     });
 
     material.unique_cooking_effect = potentialUniqueCookingEffect[0];
 
-    return this.repo.save(material);
+    material.common_locations = potentialCommonLocations;
+
+    return this.repoMaterial.save(material);
   }
 
   find() {
-    return this.repo.find({
+    return this.repoMaterial.find({
       select: {
         id: true,
         name: true,
@@ -77,29 +76,50 @@ export class MaterialsService {
         fuse_attack_power: true,
         hearts_recovered: true,
         unique_cooking_effect: { name: true },
-        common_locations: true,
+        common_locations: { name: true },
         tradeable: true,
       },
       relations: {
         unique_cooking_effect: true,
+        common_locations: true,
       },
     });
   }
 
   findOne(id: number) {
-    return this.repo.findOneBy({ id });
+    return this.repoMaterial.findOneBy({ id });
   }
 
-  async update(id: number, attrs: Partial<Material>) {
+  async update(id: number, attrs: Partial<UpdateMaterialDto>) {
     const material = await this.findOne(id);
 
     if (!material) {
       throw new NotFoundException();
     }
 
+    const unique_cooking_effect =
+      attrs.unique_cooking_effect &&
+      (await this.srvcUniqueCookingEffect.findByName(
+        attrs.unique_cooking_effect,
+      ));
+
+    const common_locations =
+      attrs.common_locations.length > 0 &&
+      (await Promise.all(
+        await this.srvcCommonLocation.findByName(attrs.common_locations),
+      ));
+
     Object.assign(material, attrs);
 
-    return this.repo.save(material);
+    if (attrs.unique_cooking_effect) {
+      material.unique_cooking_effect = unique_cooking_effect[0];
+    }
+
+    if (attrs.common_locations.length > 0) {
+      material.common_locations = common_locations;
+    }
+
+    return this.repoMaterial.save(material);
   }
 
   async remove(id: number) {
@@ -109,6 +129,6 @@ export class MaterialsService {
       throw new NotFoundException();
     }
 
-    return this.repo.remove(material);
+    return this.repoMaterial.remove(material);
   }
 }
